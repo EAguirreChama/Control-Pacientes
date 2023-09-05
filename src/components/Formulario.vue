b653cc707e8baa335c8ce337e6d95dcf025f635f
EAguirre <aguirreernesto25@gmail.com>
2023-09-05 11:27:00 -0300

    Actualización 2

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -85,12 +85,10 @@
             @submit.prevent="validar"
         >
             <div class="mb-5">
-                <label 
-                    for="mascota"
-                    class="block text-gray-700 uppercase font-bold"
-                >
+                <label for="mascota" class="block text-gray-700 uppercase font-bold">
                     Nombre Mascota
                 </label>
+
                 <input 
                     id="mascota"
                     type="text"
@@ -100,13 +98,12 @@
                     @input="$emit('update:nombre', $event.target.value)"
                 >
             </div>
+
             <div class="mb-5">
-                <label 
-                    for="propietario"
-                    class="block text-gray-700 uppercase font-bold"
-                >
+                <label for="propietario" class="block text-gray-700 uppercase font-bold">
                     Nombre Propietario
                 </label>
+
                 <input 
                     id="propietario"
                     type="text"
@@ -116,13 +113,12 @@
                     @input="$emit('update:propietario', $event.target.value)"
                 >
             </div>
+
             <div class="mb-5">
-                <label 
-                    for="email"
-                    class="block text-gray-700 uppercase font-bold"
-                >
+                <label for="email" class="block text-gray-700 uppercase font-bold">
                     Correo Electronico
                 </label>
+
                 <input 
                     id="email"
                     type="email"
@@ -132,13 +128,12 @@
                     @input="$emit('update:email', $event.target.value)"
                 >
             </div>
+
             <div class="mb-5">
-                <label 
-                    for="alta"
-                    class="block text-gray-700 uppercase font-bold"
-                >
+                <label for="alta" class="block text-gray-700 uppercase font-bold">
                     Alta
                 </label>
+
                 <input 
                     id="alta"
                     type="date"
@@ -147,13 +142,12 @@
                     @input="$emit('update:alta', $event.target.value)"
                 >
             </div>
+
             <div class="mb-5">
-                <label 
-                    for="sintomas"
-                    class="block text-gray-700 uppercase font-bold"
-                >
+                <label for="sintomas" class="block text-gray-700 uppercase font-bold">
                     Síntomas
                 </label>
+
                 <textarea
                     id="sintomas"
                     placeholder="Describe los síntomas del paciente"
