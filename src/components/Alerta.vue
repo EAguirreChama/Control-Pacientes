42abe258ccae0a4d0d37a81570b95e505ce4c3d6
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-13 14:00:52 -0300

    Update Alerta.vue

--- src/components/Alerta.vue
+++ src/components/Alerta.vue
@@ -1,5 +1,4 @@
 <script setup>
-
     import { computed } from 'vue';
     const props = defineProps({
         alerta: {
@@ -11,16 +10,13 @@
     const isError = computed(() => {
         return  props.alerta.tipo === 'error'
     })
-
 </script>
 
 <template>
-
     <div 
         :class="[isError ? 'bg-red-500' : 'bg-green-500']"
         class="text-white text-center p-3 uppercase font-bold mb-3 rounded-md"
     >
         {{ alerta.mensaje }}
     </div>
-    
 </template>
