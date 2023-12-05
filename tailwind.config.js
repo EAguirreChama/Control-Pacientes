b6c6a2ef61f8255b37443fe381a225e76dc06709
EAguirre <aguirreernesto25@gmail.com>
2023-12-05 19:29:53 -0300

    Instalando TailwindCss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 export default {
   content: [
     "./index.html",
+    // Lo que hace la sintaxis ** hace de que entre en cada carpeta para aplicar los estilos de este framework
     "./src//**/*.{vue,js,ts,jsx,tsx}"
   ],
   theme: {
